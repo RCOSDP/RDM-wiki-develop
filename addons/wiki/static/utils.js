6081aad29eeabcebafa7f832c76451284ff1a532
NishikawaKoharu <nishikawa@omik.co.jp>
2025-02-07 19:18:27 +0900

    ChangeLogic

--- addons/wiki/static/utils.js
+++ addons/wiki/static/utils.js
@@ -59,7 +59,7 @@ export function flatMap(ast, fn) {
                     //Mod Start__
                     //node.children = [underline]
                     // 以降のデータも詰め込む
-                    if(cnt < node.children.length){
+                    if(cnt < node.children.length-1){
                         //const tailChildren = []
                         //tailChildren.concat(node.children.slice(cnt,-1))
                         const tailChildren = node.children.slice(cnt+1)
@@ -123,7 +123,7 @@ export function flatMap(ast, fn) {
                     //    const xs2 = transform(tailChildren, 0, colorText)
                     //    node.children = node.children.concat(xs2)
                     //}
-                    if(cnt<node.children.length){
+                    if(cnt<node.children.length-1){
                         //const tailChildren = []
                         const tailChildren = node.children.slice(cnt+1)
                         //tailChildren.concat(node.children.slice(cnt,-1))
