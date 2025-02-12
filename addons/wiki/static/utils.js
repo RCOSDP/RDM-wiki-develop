90539278863bd21700023602eb9f7e09b6719976
NishikawaKoharu <nishikawa@omik.co.jp>
2025-02-12 14:15:07 +0900

    ChangeLogic

--- addons/wiki/static/utils.js
+++ addons/wiki/static/utils.js
@@ -29,13 +29,17 @@ export function flatMap(ast, fn) {
                     var tmp = "<span style=\"color: " + colorName + "\"><\/span>"
                     test.push({ type: 'text' ,value : tmp})
                     test.push({ type: 'text' ,value : node.children[i].value.replace(tmp, '')})
+                }else if(node.children[i] && node.children[i].type === 'text' && /<u>/.test(node.children[i].value) && /<\/u>/.test(node.children[i].value)){
+                    //if(/<u>/.test(node.children[i].value))
                 }else{
                     test.push({ type: node.type ,children : node.children[i]})
                 }
             }
             node = test
             //nishi
-            if (node.children[0] && node.children[0].type === 'text' && /<u>/.test(node.children[0].value)) {
+            //if (node.children[0] && node.children[0].type === 'text' && /<u>/.test(node.children[0].value)) {
+            if (node.children[0] && node.children[0].type === 'text')
+                if(/<u>/.test(node.children[0].value)) {
                 // 下線が存在する場合
                 var cnt = 0
                 for(var i = 0 ; i < node.children.length ; i++) {
@@ -86,6 +90,7 @@ export function flatMap(ast, fn) {
                     }
                     node.children = [underline]
                     //Mod End
+                }
                 }
             }else if(node.children[0] && node.children[0].type === 'text' && /<span style=\"color/.test(node.children[0].value)) {
                 // 文字色が存在する場合
