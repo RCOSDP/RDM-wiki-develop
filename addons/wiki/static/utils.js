f4db7e0a282782a2361d85aaa8cc04a6024974f8
NishikawaKoharu <nishikawa@omik.co.jp>
2025-03-11 15:59:06 +0900

    childlen

--- addons/wiki/static/utils.js
+++ addons/wiki/static/utils.js
@@ -252,18 +252,18 @@ export function flatMap(ast, fn) {
         if((node.children[0].value.match(/\*\*\*\</g) || []).length === 1){
             //太文字とイタリックがある
             var stEmpChildren =[]
+            stEmpChildren= ({ type: 'strong' })
             stEmpChildren.children = strChildren
-            stEmpChildren.push({ type: 'strong' })
-            remainingChildren.push({ type: 'emphasis' })
+            remainingChildren = ({ type: 'emphasis' })
             remainingChildren.children = [stEmpChildren]
         }else if((node.children[0].value.match(/\*\*\</g) || []).length === 1){
             //イタリックがある
-            remainingChildren.children = [strChildren]
             remainingChildren = ({ type: 'emphasis'})
+            remainingChildren.children = [strChildren]
         }else if((node.children[0].value.match(/\*\</g) || []).length === 1){
             //太文字だけある
-            remainingChildren.children = [strChildren]
             remainingChildren = ({ type: 'strong'})
+            remainingChildren.children = [strChildren]
         }else{
             //何もない
             return
