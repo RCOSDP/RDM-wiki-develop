2dbdc8a95090fc4a063cf8ea48638435a2152040
NishikawaKoharu <nishikawa@omik.co.jp>
2025-03-13 08:50:25 +0900

    children

--- addons/wiki/static/utils.js
+++ addons/wiki/static/utils.js
@@ -268,6 +268,10 @@ export function flatMap(ast, fn) {
 
         if(strChildren){
             remainingChildren.push({type: 'text' , value: frontStr})
+            //nishi Add Start
+            var tagStartStr = node.children[0].value.replace(/\*{1,3}\</,'<')
+            remainingChildren.push({type: 'text' , value: tagStartStr})
+            //nishi Add End
             if((node.children[0].value.match(/\*\*\*\</g) || []).length === 1){
                 //太文字とイタリックがある
                 var stEmpChildren =[]
@@ -290,6 +294,10 @@ export function flatMap(ast, fn) {
             if(remainingChildren2 !== ""){
                 remainingChildren.push(remainingChildren2)
             }
+            //nishi Add Start
+            var tagEndStr = node.children[0].value.replace(/\>\*{1,3}/,'>')
+            remainingChildren.push({type: 'text' , value: tagEndStr})
+            //nishi Add End
             remainingChildren.push({type: 'text' , value: tailStr})
 
             // ノードの２番目に挿入
