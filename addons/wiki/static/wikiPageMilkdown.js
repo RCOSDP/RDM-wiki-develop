f9ee740c4fe436c528f9b3281c28902062c44cce
NishikawaKoharu <nishikawa@omik.co.jp>
2025-03-14 18:12:27 +0900

    Link

--- addons/wiki/static/wikiPageMilkdown.js
+++ addons/wiki/static/wikiPageMilkdown.js
@@ -1223,4 +1223,15 @@ var WikiPageMilkdown = function(selector, options) {
     });
 };
 
+//nishi
+window.addEventListener('load', function() {
+    // URLのアンカー（#以降の部分）を取得
+    var urlHash = location.hash;
+    console.log('Test1' + urlHash);
+    // URLにアンカーが存在する場合
+    if(urlHash){
+        window.location.hash = urlHash
+    }
+  });
+//nishi
 export default WikiPageMilkdown;
