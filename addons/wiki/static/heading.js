1dbd9bf4f73f04842825b9d9415981dda7ee8a9b
NishikawaKoharu <nishikawa@omik.co.jp>
2025-03-31 16:19:21 +0900

    regularExpression

--- addons/wiki/static/heading.js
+++ addons/wiki/static/heading.js
@@ -3,8 +3,7 @@
 export function customHeadingIdGenerator(node) {
     return node.textContent
         .toLowerCase()
+        .replace(/[^\p{L}\p{N}._:;-]/gu, '')
         .replace(/\s+/g, '')
         .trim();
 }
-
-//        .replace(/[^\p{L}\p{N}._:;-]/gu, '')
