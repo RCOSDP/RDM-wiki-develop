3a277c250f49a77fef7b443a65b3ed0c74ef4e21
NishikawaKoharu <nishikawa@omik.co.jp>
2025-04-10 15:22:02 +0900

    inlineColor34

--- addons/wiki/static/utils.js
+++ addons/wiki/static/utils.js
@@ -235,7 +235,7 @@ export function flatMap(ast, fn) {
                 if (openTags.length > 0) {remainingChildren.push({ type: 'text', value: openTags})}
 //                if(startCnt === 0){
                     //remainingChildren = remainingChildren.concat(node.children.slice(startCnt + 1,startCnt + endCnt))
-                    remainingChildren = remainingChildren.concat(node.children.slice(startCnt + 1, endCnt + 1))
+                    remainingChildren = remainingChildren.concat(node.children.slice(startCnt + 1, endCnt))
 //                }else{
 //                    remainingChildren = remainingChildren.concat(node.children.slice(startCnt + 1,startCnt + endCnt -1))
 //                }
